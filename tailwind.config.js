dd3ea102b905652c02c2da4e3f984e3d6d09209c
bimodwilaksono <bimodwilaksono@gmail.com>
2022-06-16 08:18:14 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inconsolata: "Inconsolata",
+        spacemono: "Space Mono",
+        montserrat: "Montserrat",
+      },
+    },
+  },
+  plugins: [],
+};
